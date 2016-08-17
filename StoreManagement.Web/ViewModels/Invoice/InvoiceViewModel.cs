ae670c5a7622628b57f4a0c5b0a2b0b1e4293103
AliMF <AliMF@WSN-504>
2016-08-17 10:57:34 +0430

    view model except product added

---
+++ StoreManagement.Web/ViewModels/Invoice/InvoiceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreManagement.Web.ViewModels.Invoice
+{
+    public class InvoiceViewModel
+    {
+        public string Number { get; set; }
+
+        public byte[] Version { get; set; }
+
+        public Customer.CustomerViewModel Customer { get; set; }   // needs check
+
+        ICollection<InvoiceItem.InvoiceItemViewModel> Items { get; set; }
+    }
+}
