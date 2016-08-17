c7cca21a5a7528078c982786855f5faeb98aa68c
afshinh <webmaster.afshin@gmail.com>
2016-08-17 11:08:05 +0430

    Implement Some Actions of CustomerController And Add CustomerService

---
+++ StoreManagement.Web/Controllers/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using StoreManagement.Web.Models;
+using StoreManagement.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreManagement.Web.Controllers
+{
+    [RoutePrefix("Customer")]
+    [Route("{action}")]
+    public class CustomerController : Controller
+    {
+        #region Fields
+        /// <summary>
+        /// todo : use dependecy injection
+        /// </summary>
+        private readonly ICustomerService _customerService;
+        #endregion
+
+        #region Constructor
+        public CustomerController(ICustomerService customerSevice)
+        {
+            _customerService = customerSevice;
+        }
+
+        public CustomerController()
+        {
+        }
+        #endregion
+
+        #region List
+        public ActionResult List()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var customers = db.Customers.Select(customer => new CustomerViewModel { })
+                    .ToList();
+            }
+
+            return View(customers);
+        }
+        #endregion
+
+        #region Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StoreManagement.Web/Services/CustomerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreManagement.Web.Services
+{
+    public class CustomerService : ICustomerService
+    {
+    }
+}
