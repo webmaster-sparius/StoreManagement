c6f73758a39d53cba087b15e091f48ead024ba48
RonakR <roonak.r74@gmail.com>
2016-08-22 13:20:04 +0430

    some changes in views for make language Persian

--- StoreManagement.Web/ViewModels/Product/AddProductViewModel.cs
+++ StoreManagement.Web/ViewModels/Product/AddProductViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using StoreManagement.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
@@ -14,15 +15,20 @@ namespace StoreManagement.Web.ViewModels.Product
         [Required(ErrorMessage = "نام کالا را وارد کنید.")]
         [StringLength(100, ErrorMessage = "نام کالا نباید بیشتر از 100 کاراکتر باشد.")]
         [Remote("UniqueCategoryTitle", ErrorMessage = "یک کالا با این نام قبلا در سیستم ثبت شده است.", HttpMethod = "POST")]
+        [DisplayName("نام")]
         public string Name { get; set; }
         [Required(ErrorMessage = "کد کالا را وارد کنید.")]
         [StringLength(50, ErrorMessage = "تعداد کاراکترهای کد کالا نباید بیشتر از 50کاراکتر باشد.")]
         [Remote("UniqueCategoryTitle", ErrorMessage = "یک کالا با این کد قبلا در سیستم ثبت شده است.", HttpMethod = "POST")]
+        [DisplayName("کد")]
         public string Code { get; set; }
         [Required(ErrorMessage = "قیمت کالا را وارد کنید.")]
+        [DisplayName("قیمت")]
         public decimal Price { get; set; }
+        [DisplayName("توضیحات")]
         public string Description { get; set; }
 
+        [DisplayName("گروه")]
         [Required(ErrorMessage ="گروه کالا را انتخاب کنید.")]
         public long CategoryId { get; set; }
 
