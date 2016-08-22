c98865dcc48c44d52b5216ee1b20725dd9353cf8
Mohammad Dehghan <md.unicorn@gmail.com>
2016-08-22 14:51:22 +0430

    EditProductViewModel code formatted.

--- StoreManagement.Web/ViewModels/Product/EditProductViewModel.cs
+++ StoreManagement.Web/ViewModels/Product/EditProductViewModel.cs
@@ -15,26 +15,33 @@ namespace StoreManagement.Web.ViewModels.Product
     {
         [Required]
         public long Id { get; set; }
+
         [Required(ErrorMessage = "نام کالا را وارد کنید.")]
         [StringLength(50, ErrorMessage = "نام کالا نباید بیشتر از 100 کاراکتر باشد.")]
         [Remote("UniqueCategoryTitle",AdditionalFields ="Id",ErrorMessage = "یک کالا با این نام قبلا در سیستم ثبت شده است.", HttpMethod = "POST")]
         [DisplayName("نام")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "قیمت کالا را وارد کنید.")]
         [DisplayName("قیمت")]
         public decimal Price { get; set; }
+
         [Required(ErrorMessage = "کد کالا را وارد کنید.")]
         [StringLength(50, ErrorMessage = "تعداد کاراکترهای کد کالا نباید بیشتر از 50کاراکتر باشد.")]
         [Remote("UniqueCategoryTitle",AdditionalFields ="Id" ,ErrorMessage = "یک کالا با این کد قبلا در سیستم ثبت شده است.", HttpMethod = "POST")]
         [DisplayName("کد")]
         public string Code { get; set; }
+
         [DisplayName("توضیحات")]
         public string Description { get; set; }
+
         [Required]
         public byte[] Version { get; set; }
+
         [Required(ErrorMessage = "گروه کالا را انتخاب کنید.")]
         [DisplayName("گروه")]
         public long CategoryId { get; set; }
+
         public DbSet<Models.Category> Categories = new ApplicationDbContext().Categories;
     }
 }
