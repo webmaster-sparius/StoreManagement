8f490dd70c3981754350328dc25c6a73773f4a5c
afshinh <webmaster.afshin@gmail.com>
2016-08-29 14:19:29 +0430

    Reuse And Optimize ComponentManager

--- StoreManagement.Common/EntityServices/ICustomerService.cs
+++ StoreManagement.Common/EntityServices/ICustomerService.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using StoreManagement.Common.Models;
+using StoreManagement.Framework.Common;
 
 namespace StoreManagement.Common.EntityServices
 {
-    public interface ICustomerService
+    public interface ICustomerService : IEntityService<Customer>
     {
         bool CheckCustomerExist(string firstName, string lastName, long? id);
         IEnumerable<Customer> FetchAll();
--- StoreManagement.Common/EntityServices/IProductService.cs
+++ StoreManagement.Common/EntityServices/IProductService.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using StoreManagement.Web.Areas.BasicData.ViewModels;
+using StoreManagement.Framework.Common;
 
 namespace StoreManagement.Common.EntityServices
 {
-    public interface IProductService
+    public interface IProductService : IEntityService<Product>
     {
         bool CheckNameExist(string name, long? id);
         bool CheckCodeExist(string code, long? id);
