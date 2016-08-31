4fb263db0c48e18f05bca9694141e95728f8128c
AliMF <AliMF@WSN-504>
2016-08-31 17:07:24 +0430

    some changes

--- StoreManagement.Web/Scripts/page_edited_warning.js
+++ StoreManagement.Web/Scripts/page_edited_warning.js
@@ -1,66 +1,33 @@
-﻿var original_array = [];
+﻿
+var peo = {};
 
-$(function () {
+peo.original_array = [];
+
+$(document).ready(function () {
     $("#editable_form div.form-group :input").
-                    each(function () { original_array.push($(this).val()) });
+                    each(function () { peo.original_array.push($(this).val()) });
 });
 
 
 //window.onunload(alert("bye now"));
 
-function doc_changed() {
+function peo_doc_changed() {
     var new_array = [];
     $("#editable_form div.form-group :input").
                     each(function () { new_array.push($(this).val()) });
     var res = false;
 
-    for (var i = 0; i < original_array.length; i++) {
-        if (original_array[i] != new_array[i]) {
+    for (var i = 0; i < peo.original_array.length; i++) {
+        if (peo.original_array[i] != new_array[i]) {
             res = true;
         }
     }
     return res;
 }
 
-
-function go_for_return(cn,vn) {     // controller name , view name
-    // if something is changed
-    var changed = doc_changed();
-    if (changed) {
-        $("#dialog-confirm").dialog({
-            resizable: false,
-            height: "auto",
-            width: 400,
-            modal: true,
-            buttons: {
-                " حواسم هست ": function () {
-                    $(this).dialog("close");
-                    if(vn == 'create'){
-                        document.location = '/basicdata/' + cn + '/list';
-                    }
-                    else {
-                        document.location = '../list';
-                    }
-                },
-                " خوب شد گفتی ": function () {
-                    $(this).dialog("close");
-                }
-            }
-        });
-    }
-    else {
-        if (vn == 'create') {
-            document.location = '/basicdata/' + cn + '/list';
-        }
-        else {
-            document.location = '../list';
-        }
-    }
-}
-
 function go_for_return_to_list_of(cn) {
     // if something is changed
-    var changed = doc_changed();
+    var changed = peo_doc_changed();
     if (changed) {
         $("#dialog-confirm").dialog({
             resizable: false,
